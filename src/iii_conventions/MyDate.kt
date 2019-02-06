ec92cbeb567e70cbe9ef1c45d45b9bbf171e8133
Tom <tom.j.egan88@gmail.com>
2019-02-06 14:17:26 +0000

    Complete iii-27

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,16 +1,14 @@
 package iii_conventions
 
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
-    override fun compareTo(other: MyDate): Int {
-        return when  {
-            year != other.year -> year - other.year
-            month != other.month -> month - other.month
-            else -> dayOfMonth - other.dayOfMonth
-        }
+    override fun compareTo(other: MyDate): Int = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
     }
 }
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
@@ -18,6 +16,6 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(override val start: MyDate, override val endInclusive: MyDate) :ClosedRange<MyDate> {
+class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate> {
     override fun contains(value: MyDate): Boolean = value >= start && value <= endInclusive
 }
